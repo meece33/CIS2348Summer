7da284f8b58ef890ae84d585c49b1de8dccd5959
meece33 <mcorado33@gmail.com>
2021-06-14 01:15:10 -0500

    Done

--- Homework1/3.19.py
+++ Homework1/3.19.py
@@ -1,1 +1,24 @@
-print('hi')
+import math
+
+
+height = int(input('Enter wall height (feet):\n'))               # input statements for width and height
+width = int(input('Enter wall width (feet):\n'))
+
+area = width * height                                    # formula for area base*height
+print('Wall area:', area, 'square feet')
+
+paintNeed = area / 350
+print('Paint needed:', '{:.2f}'.format(paintNeed), 'gallons')
+
+cansNeed = math.ceil(paintNeed)                    # establishes the cans needed
+print('Cans needed:', cansNeed, 'can(s)')
+
+colorDict = {"red": 35,                 # created a dictionary with the proper prices for each color
+             "blue": 25,
+             "green": 23}
+
+color = (input('\nChoose a color to paint the wall:\n'))
+colorChoice = colorDict.get(color)
+cost = cansNeed * colorChoice
+
+print('Cost of purchasing ', color, ' paint: $', cost, sep='')
