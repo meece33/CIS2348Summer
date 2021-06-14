95e878de85492285ab2eb7b48d2e025602cb2cb0
meece33 <mcorado33@gmail.com>
2021-06-14 00:58:59 -0500

    Done

--- Homework1/1.20.py
+++ Homework1/1.20.py
@@ -1,1 +1,17 @@
-print('hi')
+# Mauricio Corado 1254732
+
+
+userNum = int(input('Enter integer:\n'))               # User enters a number
+print('You entered:', userNum)
+
+userNumSquared = userNum * userNum                    # the number entered is then squared and printed
+print(userNum, 'squared is', userNumSquared)
+
+userCubed = userNum * userNum * userNum              # here the number is cubed by simple multiplication
+print('And', userNum, 'cubed is', userCubed, '!!')
+
+userNum2 = int(input('Enter another integer:\n'))
+
+print(userNum, '+', userNum2, 'is', userNum+userNum2)
+print(userNum, '*', userNum2, 'is', userNum*userNum2)
+
