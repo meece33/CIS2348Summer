477f0b37a3a1549091b644725289c374a5187775
meece33 <mcorado33@gmail.com>
2021-06-14 00:50:40 -0500

    Done

--- Homework1/1.12ZyLab.py
+++ Homework1/1.12ZyLab.py
@@ -1,1 +1,6 @@
-print('hi')
+# Mauricio Corado 1254732
+
+userNum = int(input())
+userNumSquared = userNum * userNum    # Bug here; fix it when instructed
+
+print(userNumSquared)    # Output formatting issue here; fix it when instructed
