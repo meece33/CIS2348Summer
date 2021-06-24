58701dcc97230037173b7fae527ad98e739c2895
meece33 <mcorado33@gmail.com>
2021-06-24 01:39:31 -0500

    Complete

--- Homework2/6.22.py
+++ Homework2/6.22.py
@@ -1,1 +1,22 @@
-print('hi')
+# Mauricio Corado 1254732
+
+
+# takes input for the coefficients of both equations
+
+a = int(input())
+b = int(input())
+c = int(input())
+a2 = int(input())
+b2 = int(input())
+c2 = int(input())
+
+solution = 0
+
+for x in range(-10, 10, 1):
+    for y in range(-10, 10, 1):
+        if (a * x + b * y == c) and (a2 * x + b2 * y == c2):
+            print(x, y)
+            solution = 1
+# if solution has not been found i.e not equal to 1 as it would be assigned
+if solution != 1:
+    print("No solution")
