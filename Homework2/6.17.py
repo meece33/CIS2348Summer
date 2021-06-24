e0215736e3bc577753a6393cfab319e80084d6d5
meece33 <mcorado33@gmail.com>
2021-06-24 01:36:59 -0500

    Complete

--- Homework2/6.17.py
+++ Homework2/6.17.py
@@ -1,1 +1,15 @@
-print('hi')
+# Mauricio Corado 1254732
+
+# Receives user input
+password = input()
+
+# phrase used to replace old with the new
+# Replace method was the easiest to use
+
+password = password.replace('i', '!')
+password = password.replace('a', '@')
+password = password.replace('m', 'M')
+password = password.replace('B', '8')
+password = password.replace('o', '.')
+password = password+'q*s'
+print(password)
