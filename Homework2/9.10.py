756f15e98af86b0f9f51ecfe7907ffee94708776
meece33 <mcorado33@gmail.com>
2021-06-24 03:06:53 -0500

    Complete

--- Homework2/9.10.py
+++ Homework2/9.10.py
@@ -1,1 +1,14 @@
-print('hi')
+# Mauricio Corado 1254732
+
+import csv                      # import csv as needed
+file_name = input()
+file = open(file_name, 'r')
+read = csv.reader(file)             # reads file into read variable
+
+for line in read:                       # for loop so that it goes through the list
+    word_list = line   # original word list with duplicates
+    no_dupl = list(dict.fromkeys(word_list))    # crates a new list with no duplicates from the original list
+
+    for i in range(len(no_dupl)):
+        print(no_dupl[i], word_list.count(no_dupl[i]))
+# for loop iterates over the no duplicate list and prints the word count from word list which is the original
