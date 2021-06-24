36d758c4829f6ef480d8744cc6a505ee80b23a67
meece33 <mcorado33@gmail.com>
2021-06-24 01:54:11 -0500

    Complete

--- Homework2/8.10.py
+++ Homework2/8.10.py
@@ -1,1 +1,12 @@
-print('hi')
+# Mauricio Corado 1254732
+
+user_input = input()             # takes in input
+
+no_spaces = user_input.replace(" ", "")           # replaces spaces with no spaces
+rev = ''.join(reversed(no_spaces))                # reversed the no spaced string
+
+# if statement to determine if indeed the string is a palindrome
+if no_spaces == rev:
+    print(user_input, 'is a palindrome')
+else:
+    print(user_input, 'is not a palindrome')
