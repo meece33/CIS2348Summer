ec989ba11cf8fea7f57b4ccfaa03b5d60df5ca6d
meece33 <mcorado33@gmail.com>
2021-06-28 02:17:31 -0500

    Complete

--- Homework3/11.22.py
+++ Homework3/11.22.py
@@ -1,1 +1,11 @@
-print('hi')
+# Mauricio Corado 1254732
+
+word_list = [str(i) for i in input().split()]         # gets user input in string format and stores it as a list
+
+count = 0                                   # count is used in order to know when to exit loop
+
+# basic loop that iterates through list and prints deliverable as needed
+
+while count < len(word_list):
+    print('{} {}'.format(word_list[count], word_list.count(word_list[count])))
+    count = count + 1
