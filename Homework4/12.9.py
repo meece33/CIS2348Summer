01e8fea8a7409e4880d12ee6d2636fa7bfbb1fbb
meece33 <mcorado33@gmail.com>
2021-07-07 22:53:00 -0500

    Complete

--- Homework4/12.9.py
+++ Homework4/12.9.py
@@ -1,1 +1,14 @@
-print('hi')
+# 1254732 Mauricio Corado
+
+user_input = input().split()     # splits the user input
+name = user_input[0]            # set the first part of the split into name
+age = user_input[1]         # second part of the input is age
+
+while name != '-1':     # loo[ that runs until value is -1
+    try:
+        age = int(user_input[1]) + 1
+    except ValueError:
+        age = 0
+    print('{} {}'.format(name, age))
+    user_input = input().split()
+    name = user_input[0]
