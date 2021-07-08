837dc787be982e69de5cbe975d8c21d9dd330c71
meece33 <mcorado33@gmail.com>
2021-07-07 21:43:41 -0500

    Complete

--- Homework4/12.7.py
+++ Homework4/12.7.py
@@ -1,1 +1,25 @@
-print('hi')
+# 1254732 Mauricio Corado
+least = 18
+most = 75
+
+
+def get_age():                      # get age method uses to error check the age parameters
+    age = int(input())
+
+    if (least > age) or (age > most):
+        raise ValueError('Invalid Age.')
+    return age
+
+
+def fat_burning_heart_rate(age):          # method used to calculate the equation needed
+    hr = ((220 - age) * .7)
+    return hr
+
+if __name__ == "__main__":      # main code checks for value error
+    try:
+        age = get_age()
+        hr = fat_burning_heart_rate(age)
+        print('Fat burning hear rate for a {} year-old: {} bpm'.format(age, hr))
+    except ValueError as wrong:
+        print(wrong)
+        print('Could not calculate heart rate info.')
